1910698c1a66a3c68d9cce69d6d84601d8969c21
Luddegoyoda <Luddegoyoda@gmail.com>
2024-02-06 09:41:06 +0100

    Final Tuesday

--- Tasmaniandevil/assets/js/index.js
+++ Tasmaniandevil/assets/js/index.js
@@ -1,21 +1,7 @@
-const element = document.getElementById("clickMeText");
-const image = document.getElementById("StarImage");
+const image = document.getElementById("logoImage");
 const body = document.querySelector("body");
 
-function ImageChange(){
-
-    if(image.src == "http://127.0.0.1:5500/assets/images/GOG_Hero.png"){
-        image.src = "assets/images/Hero2.jpg";
-    }
-    else{
-        image.src = "assets/images/GOG_Hero.png";
-    }
-}
-
-image.addEventListener("click", ImageChange)
-
-
-element.addEventListener("click", function() {
+image.addEventListener("click", function() {
     if (body.classList.contains("darkmode"))
     {
         body.classList.remove("darkmode");
